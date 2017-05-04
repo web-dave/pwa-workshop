28700419945ec52e0653072529d36634ba76da20
Dave Uncle <Dave Uncle>
2017-05-04 22:27:18 +0200

    actions

--- build/sw.js
+++ build/sw.js
@@ -40,3 +40,43 @@ self.addEventListener('push', function (event) {
 
   self.registration.showNotification(notificationData.title, notificationData);
 })
+
+self.addEventListener('notificationclick', function (event) {
+  log('[Service Worker]: Received notificationclick event', event)
+
+  event.notification.close()
+
+  if (event.action == 'opentweet') {
+    log('[Service Worker]: Performing action opentweet')
+
+    event.waitUntil(
+      clients.openWindow(event.notification.data).then(function (windowClient) {
+        // do something with the windowClient.
+      })
+    )
+  } else {
+    log('[Service Worker]: Performing default click action')
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(
+
+      clients.matchAll({
+        includeUncontrolled: true,
+        type: 'window'
+      })
+        .then(function (clientList) {
+          for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i]
+            if (client.url == '/' && 'focus' in client)
+              return client.focus()
+          }
+          if (clients.openWindow)
+            return clients.openWindow('/')
+        }))
+  }
+})
+
+self.addEventListener('notificationclose', function (event) {
+  log('[Service Worker]: Received notificationclose event')
+})
