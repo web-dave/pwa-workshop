f787fa04f2a2a42ee0270f86fa44d7e1657374a7
web-dave <dave@webdave.de>
2017-05-04 08:10:55 +0200

    Forcing SW to skip waiting the update

--- build/sw.js
+++ build/sw.js
@@ -1,30 +1,9 @@
 var log = console.log.bind(console);
 var err = console.error.bind(console);
 
-var version = '2';
-var cacheName = 'pwa-freitagtraining-v' + version;
-var appShellFilesToCache = [
-  './',
-  './index.html',
-  './inline.9b502c297169a277d5fb.bundle.js',
-  './main.7076b51032c778e38a8b.bundle.js',
-  './polyfills.a826c616902ba35dd8ef.bundle.js',
-  './styles.b0e114c891ad3843b53e.bundle.css',
-  './vendor.331882c5745789983b16.bundle.js',
-  './favicon.ico',
-  './assets/images/logo.png'
-];
-
 self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting());
   log('[Service Worker]: Installed');
-
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      log('[Service Worker]: Caching App Shell');
-      return cache.addAll(appShellFilesToCache);
-    })
-  );
 });
 
 self.addEventListener('activate', (event) => {
